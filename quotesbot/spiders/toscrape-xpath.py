b068f2d49b89720001fab552031b2401a0fd1fbd
mccree <song_ji@worksap.co.jp>
2017-12-11 19:15:22 +0800

    Login in product-ci gitlab

--- quotesbot/spiders/toscrape-xpath.py
+++ quotesbot/spiders/toscrape-xpath.py
@@ -5,7 +5,6 @@ import scrapy
 class ToScrapeSpiderXPath(scrapy.Spider):
     name = 'toscrape-xpath'
     urls = [
-        'http://www.baidu.com',
         'https://scm.hue.workslan/users/sign_in',
         'http://product-ci/users/sign_in'
     ]
@@ -16,7 +15,16 @@ class ToScrapeSpiderXPath(scrapy.Spider):
 
     def login(self, response):
         self.log("Login start:")
-        self.log("Start is....")
+        self.log(response.body)
+        authenticity_token= response.xpath("//meta[@name='csrf-token']/@content")[0].extract()
+        print 'token:'+authenticity_token
+        frmdata={'utf8': '✓', 'authenticity_token': authenticity_token, 'user[login]': 'song_ji@worksap.co.jp',
+                 'user[password]': 'ben321ben', 'user[remenber_me]': '0'}
+        yield scrapy.FormRequest(url=response.url, callback=self.parse_gitlab, formdata=frmdata)
+
+    def parse_gitlab(self, response):
+        print response.body
+
 
     def parse(self, response):
         for quote in response.xpath('//div[@class="quote"]'):
